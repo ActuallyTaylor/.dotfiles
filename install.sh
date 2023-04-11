1c87d1d1addb33a2148530618fee92e09619324f
ActuallyTaylor <zachary.lineman@gmail.com>
2023-04-11 16:42:21 -0400

    Moved everything around

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ ln -s ~/.dotfiles/.zshprofile ~/.zshprofile
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vim ~/.vim
-ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/.dotfiles/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-ln -s ~/.dotfiles/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -s ~/.dotfiles/waybar ~/.config/waybar
+ln -s ~/.dotfiles/linux/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/.dotfiles/linux/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -s ~/.dotfiles/linux/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -s ~/.dotfiles/linux/waybar ~/.config/waybar
