2e1349e14d01a349b067f6cdbf31514b731f835b
ActuallyTaylor <zachary.lineman@gmail.com>
2023-04-13 15:56:10 -0400

    Updated keybinds and wallpaper script

--- macos/wallpapers.zsh
+++ macos/wallpapers.zsh
@@ -5,10 +5,12 @@ WALLPAPER=""
 
 if [[ $appearance != 'Dark' ]]; then
     echo "Light Mode"
-    WALLPAPER="~/.dotfiles/wallpapers/girl_smiling.jpg"    
+    WALLPAPER="~/.dotfiles/wallpapers/girl_smiling.jpg"   
+    #echo -e "\033]50;SetProfile=Light\a" 
 else
     echo "Dark Mode"
     WALLPAPER="~/.dotfiles/wallpapers/girl_standing.png"
+    #echo -e "\033]50;SetProfile=Light\a"
 fi
 
 for x in 1 2 3 4 5 6 7 8 9 0; do \ 
@@ -16,6 +18,9 @@ for x in 1 2 3 4 5 6 7 8 9 0; do \
     osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${WALLPAPER}\"";
 done;
 
+
+echo "Updating..."
 yabai -m space --focus 1
-#brew services restart sketchybar
-sketchybar --update
+brew services stop sketchybar
+brew services start sketchybar
+echo "Done Updating!"
