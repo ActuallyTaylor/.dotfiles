2f6fa3beaf9a5fbad7ae6e1569c3233792dd251f
Taylor Lineman <taylor.lineman@gmail.com>
2024-09-11 13:01:15 -0400

    Added zed to config

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-ln -s ~/.dotfiles/nvim/ ~/.config/nvim/
+#!/bin/bash
+
+if [ ! -L ~/.config/nvim ] ; then
+    echo "Creating symlink for nvim"
+    ln -s ~/.dotfiles/nvim/ ~/.config
+else
+    echo "Nvim symlink already exists"
+fi
+
+
+if [ ! -L ~/.config/zed ]; then
+  echo "Creating symlink for zed"
+  ln -s ~/.dotfiles/zed/ ~/.config
+else
+  echo "Zed symlink already exists"
+fi
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,18 @@
 -- Utility function to load a .env file
 local function load_dotenv(dotenv_path)
-  local file = io.open(dotenv_path, "r")
-  if not file then
-    return
-  end
-
-  for line in file:lines() do
-    local key, value = line:match("^([%w_]+)=([%w_./:-]+)$")
-    if key and value then
-      vim.fn.setenv(key, value)
+    local file = io.open(dotenv_path, "r")
+    if not file then
+        return
     end
-  end
 
-  file:close()
+    for line in file:lines() do
+        local key, value = line:match("^([%w_]+)=([%w_./:-]+)$")
+        if key and value then
+            vim.fn.setenv(key, value)
+        end
+    end
+
+    file:close()
 end
 
 -- Call the function with your .env file path
